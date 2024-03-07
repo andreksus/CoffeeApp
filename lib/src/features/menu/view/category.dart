14003a7197d6f3e5b369fc88ea7fc627b4849df0
Андрей Барышев <baryshev_2002@inbox.ru>
2024-03-07 22:59:55 +0600

    Added state for categories

--- lib/src/features/menu/view/category.dart
+++ lib/src/features/menu/view/category.dart
@@ -4,55 +4,48 @@ class Category extends StatefulWidget {
   const Category({super.key, required this.category});
   final String category;
 
-
   @override
   State<Category> createState() => _Category();
 }
 
 class _Category extends State<Category> {
+  late Color _textColor;
+  late Color _backColor;
+
+  @override
+  void initState() {
+    //Начальное значение цвета кнопки
+    _textColor = Colors.black;
+    _backColor = Colors.white;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    Widget isNotActiveWidget = Container(
-      padding: const EdgeInsets.all(8.0),
-      margin: const EdgeInsets.only(right: 8),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: Text(widget.category,
-          style: const TextStyle(
-            fontSize: 14,
-          )),
-    );
-
-    Widget isActiveWidget = Container(
-      padding: const EdgeInsets.all(8.0),
-      margin: const EdgeInsets.only(right: 8),
-      decoration: BoxDecoration(
-        color: const Color(0xff85C3DE),
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: Text(widget.category,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 14,
-          )),
-    );
-
-    Widget actualWidget = isActiveWidget;
-
     return GestureDetector(
-      child: actualWidget,
-      onTap: () => setState(() {
-        debugPrint('object');
-        if (actualWidget == isNotActiveWidget) {
-          actualWidget = isActiveWidget;
-          debugPrint('1');
-        } else {
-          actualWidget = isNotActiveWidget;
-          debugPrint('2');
-        }
-      }),
+      onTap: () {
+        setState(() {
+          if (_textColor == Colors.black) {
+            _textColor = Colors.white;
+            _backColor = const Color(0xff85C3DE);
+          } else {
+            _textColor = Colors.black;
+            _backColor = Colors.white;
+          }
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: _backColor,
+          borderRadius: BorderRadius.circular(16.0),
+        ),
+        child: Text(widget.category,
+            style: TextStyle(
+              color: _textColor,
+              fontSize: 14,
+            )),
+      ),
     );
   }
 }
